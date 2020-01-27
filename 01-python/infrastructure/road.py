d3c001fd50c8f050ca58671ccb3f2ba0380a8f00
Lance Galletti <lgallett@redhat.com>
2020-01-27 16:54:52 -0500

    Adding roads to the city

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,20 @@
-def draw_road():
-    print("road not found")
+def draw_road(length=10, axis="horizontal"):
+    VALID = ["horizontal", "vertical"]
+    if axis not in VALID:
+        # raise error
+        raise ValueError("your axis is undefined")
+    if length <0:
+        raise ValueError("your length is undefined")
+
+    if axis=="horizontal":
+        return draw_road_horizontal(length)
+    else:
+        for i in range(length):
+            print("||")
+    print()
+    return
+
+def draw_road_horizontal(length):   
+    for i in range(length):
+        print("=", end="")
     return
